f997b42ac11bc4dde16d22db594ac8ebeadc476d
Diego Rosa dos Santos <www.diegosantos.com.br@gmail.com>
2015-08-25 15:11:13 -0300

    - configurando plugins js

--- web/controllers/home.php
+++ web/controllers/home.php
@@ -4,7 +4,8 @@ use Symfony\Component\HttpFoundation\Request;
 $app->get('/', function (Request $request) use ($app) {
     $params = [
         'title' => 'Home',
-        'basePath' => $request->getBasePath()
+        'basePath' => $request->getBasePath(),
+        'email' => 'tecnologia@excellencetecnologia.com.br',
     ];
 
     return $app["mustache"]->render("home", $params);
